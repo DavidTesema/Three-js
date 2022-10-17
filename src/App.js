a826f0bb993a5425635d2562e87d7dbc59f34bbf
David Tassama <davidtassama464@gmail.com>
2022-10-18 00:09:41 +0300

    18-10-22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import Box from './comps/Box';
+import { OrbitControls } from '@react-three/drei';
+import Circle from './comps/Circle';
+import Iphone from "./comps/Iphone"
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <Canvas style={{height:"500px"}}>
+      <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2,5,2]} intensity={1}/>
+      <Box/>
+      </Canvas>
+      <Canvas style={{height:"500px"}}>
+      <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2,5,2]} intensity={1}/>
+      <Circle/>
+      </Canvas>
+      <Canvas style={{height:"500px"}}>
+      <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2,5,2]} intensity={1}/>
+      <Iphone/>
+      </Canvas>
+      </div>
+  )
 }
-
-export default App;
